56894e6313c83219e9942947428486a563cb53e8
卢成彪 <993925668@qq.com>
2019-04-29 10:17:10 +0800

    modified weak_callback_with_qobject.h

--- common_project/tool_kits/common/weak_callback/weak_callback_with_qobejct.h
+++ common_project/tool_kits/common/weak_callback/weak_callback_with_qobejct.h
@@ -1,5 +1,6 @@
 #ifndef __COMMON_WEAK_CALLBACK_WEAK_CALLBACK_WITH_QOBJECT_H__
 #define __COMMON_WEAK_CALLBACK_WEAK_CALLBACK_WITH_QOBJECT_H__
+#include <mutex> // for std::once_flag
 #include <QObject>
 #include <QPointer>
 #include "weak_callback.h"
@@ -27,15 +28,27 @@
 *
 * step 3 -- use:
 * auto weak = weak_callback_.WeakPtr();
-* someObject->RegisterCallback(weak_callback_.ToWeakCallback([=](...){
+* someAsyncRoutineWithCallback(weak_callback_.ToWeakCallback([=](...){
 * weak.lock()->EmitClosureToHostThread([=]{
 *	// do something in the thread of MyClass's host.
 *	});
 * }));
 *
+* or:
+* auto weak = weak_callback_.WeakPtr();
+* someAsyncRoutineWithCallback([=](...){
+* auto locked = weak.lock();
+* if (locked)
+* {
+*	locked->EmitClosureToHostThread([=]{
+*		// do something in the thread of MyClass's host.
+*	});
+* }
+* });
+*
 * explanation:
 * auto weak = weak_callback_.WeakPtr();
-* someObject->RegisterCallback(weak_callback_.ToWeakCallback([=](...){ // here must capture the weak variable by value[=]or[weak] NOT by reference[&]or[&weak]
+* someAsyncRoutineWithCallback(weak_callback_.ToWeakCallback([=](...){ // here must capture the weak variable by value[=]or[weak] NOT by reference[&]or[&weak]
 * // maybe called in a thread not same as MyClass's host.
 * // weak.lock() definitely is a valid shared_ptr since the callback has been called and performed here.
 * // emit a closure which will be called in the thread of MyClass's host.
@@ -57,21 +70,24 @@ namespace wcb
 	public:
 		QObjectSupportWeakCallback(QObject* host)
 		{
-			qRegisterMetaType<StdClosure>("StdClosure");
-
+			static std::once_flag once;
+			std::call_once(once, []{
+				qRegisterMetaType<StdClosure>("StdClosure"); // register StdClosure into Qt meta-object system once.
+			});
+			
 			//connect to a functor, with a "context" object defining in which event loop is going to be executed
 			QObject::connect(this, &QObjectSupportWeakCallback::Closure, host, [](const StdClosure& closure){ if (closure) { closure(); } });
 		}
 
 		QObjectSupportWeakCallback()
-			: QObjectSupportWeakCallback(this)
+		: QObjectSupportWeakCallback(this) // delegate to the ctor above
 		{}
 
 		inline void	EmitClosureToHostThread(const StdClosure& closure) { emit this->Closure(closure); }
 
 	private:
 		// it is safe that the parameter type of Closure signal is a reference -- const StdClosure&,
-		// because the parameter will be copied(QMetaType::create->QMetaType::construct->QMetaTypeFunctionHelper::Construct) if in a async/queued call(queued_activate),
+		// because the parameter will be copied(QMetaType::create->QMetaType::construct->QMetaTypeFunctionHelper::Construct) in a async/queued call(queued_activate),
 		// in a word: by reference, it is efficient in a sync call and Qt makes it safe in a async/queued call. 
 		Q_SIGNAL void		Closure(const StdClosure&);
 	};
